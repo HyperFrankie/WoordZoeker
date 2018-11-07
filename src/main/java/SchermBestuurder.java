5d5ba2b7a80f3357e63d36d6d0fc31715f9ae37a
lenardjee <lenardvdmaas@hotmail.nl>
2018-11-07 17:12:13 +0100

    added fileChooser basics

--- src/main/java/SchermBestuurder.java
+++ src/main/java/SchermBestuurder.java
@@ -1,6 +1,9 @@
 import javafx.fxml.FXML;
 import javafx.scene.control.TabPane;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 
 public class SchermBestuurder {
 
@@ -14,6 +17,12 @@ public class SchermBestuurder {
     @FXML
     public void initialize() {
         tab1Pane.getChildren().add(new WoordzoekerTabel(10, 10, 40, 40));
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Resource File");
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Text Files", "*.txt"),
+                new FileChooser.ExtensionFilter("Image Files", "*.img", "*.png", "*.jpg"));
+        File selectedFile = fileChooser.showOpenDialog(SchermStart.stage);
     }
 
     public static void postInit() {

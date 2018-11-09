ed60bcf8b5e70a269a7dcd52ad060f8c2ec814ca
lenardjee <lenardvdmaas@hotmail.nl>
2018-11-09 11:47:42 +0100

    added Img view for loaded image

--- src/main/java/SchermBestuurder.java
+++ src/main/java/SchermBestuurder.java
@@ -1,10 +1,13 @@
 import com.asprise.ocr.Ocr;
 import javafx.fxml.FXML;
 import javafx.scene.control.TabPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.FileChooser;
 
 import java.io.File;
+import java.net.MalformedURLException;
 
 public class SchermBestuurder {
 
@@ -14,6 +17,8 @@ public class SchermBestuurder {
     TabPane tabPane;
     @FXML
     AnchorPane tab1Pane;
+    @FXML
+	ImageView tab1ImgView;
 
     @FXML
     public void initialize() {
@@ -24,7 +29,7 @@ public class SchermBestuurder {
         SchermStart.stage.setMaximized(true);
     }
 
-	public void openBestandKiezer() {
+	public void openBestandKiezer() throws MalformedURLException {
 		FileChooser fileChooser = new FileChooser();
 		fileChooser.setTitle("Kies afbeelding");
 		fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Afbeeldingen", "*.img", "*.png", "*.jpg"));
@@ -32,13 +37,15 @@ public class SchermBestuurder {
 
 		System.out.println(geselecteerdBestand.toString());
 
-		Ocr.setUp();
+		tab1ImgView.setImage(new Image("file:///" + geselecteerdBestand.getPath()));
+
+		/*Ocr.setUp();
 		Ocr ocr = new Ocr(); // create a new OCR engine
 		ocr.startEngine("eng", Ocr.SPEED_SLOW); // English
 		String s = ocr.recognize(new File[] {geselecteerdBestand},
 				Ocr.RECOGNIZE_TYPE_TEXT, Ocr.OUTPUT_FORMAT_PLAINTEXT); // PLAINTEXT | XML | PDF | RTF
 		System.out.println("Result: " + s);
-		ocr.stopEngine();
+		ocr.stopEngine();*/
 	}
 
 }
